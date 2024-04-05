f88f72b93ef5f65ba71104cccd115c801c6af156
殊怀丶 <lvzhihe_123@qq.com>
2024-04-05 16:06:21 +0800

    feat: 增加t586、t610、t619

---
+++ t610/Create.sql
@@ -0,0 +1,12 @@
+drop table if exists Triangle;
+Create table If Not Exists Triangle
+(
+    x int,
+    y int,
+    z int
+);
+Truncate table Triangle;
+insert into Triangle (x, y, z)
+values ('13', '15', '30');
+insert into Triangle (x, y, z)
+values ('10', '20', '15');
---
+++ t610/Solution.sql
@@ -0,0 +1,5 @@
+select x,
+       y,
+       z,
+       IF(x + y > z and x + z > y and y + z > x, 'Yes', 'No') as triangle
+from triangle;
