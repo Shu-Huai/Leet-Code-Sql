d5da56f44729be7c67f4a06bd8aaa0a67b022bf8
殊怀丶 <lvzhihe_123@qq.com>
2024-05-04 00:09:17 +0800

    feat: 增加ps1启动脚本

---
+++ Main.ps1
@@ -0,0 +1,35 @@
+﻿$PSDefaultParameterValues['*:Encoding'] = 'utf8'
+Write-Host "请输入题号："
+$number = Read-Host
+while (-not [string]::IsNullOrWhiteSpace($number))
+{
+    $directory = "$number"
+    if (-not (Test-Path $directory -PathType Container))
+    {
+        Write-Host "输入有误，请重新输入"
+        Write-Host "请输入题号："
+        $number = Read-Host
+        continue
+    }
+    $problemFile = Join-Path -Path $directory -ChildPath "Problem.md"
+    if (Test-Path $problemFile)
+    {
+        Get-Content $problemFile
+    }
+    $mysqlServer = "mysql.lvshuhuai.cn"
+    $mysqlUser = "root"
+    $createSqlFile = Join-Path -Path $directory -ChildPath "Create.sql"
+    $solutionSqlFile = Join-Path -Path $directory -ChildPath "Solution.sql"
+    if (Test-Path $createSqlFile)
+    {
+        $createSql = Get-Content $createSqlFile
+        & mysql -e "$createSql" -h $mysqlServer -u $mysqlUser -pprwq0421 "leet_code"
+    }
+    if (Test-Path $solutionSqlFile)
+    {
+        $solutionSql = Get-Content $solutionSqlFile
+        & mysql -e "$solutionSql" -h $mysqlServer -u $mysqlUser -pprwq0421 "leet_code"
+    }
+    Write-Host "请输入题号："
+    $number = Read-Host
+}
